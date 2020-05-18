5160b033d8d7b6fdb07ff1b22e82ffafcade8948
RMJ <hayatesa@live.cn>
2020-05-18 11:22:54 +0800

    Update RentalController.js

--- application/rental/application/controllers/RentalController.js
+++ application/rental/application/controllers/RentalController.js
@@ -1,66 +1,63 @@
 'use strict'
 
 const FabricCAServices = require('fabric-ca-client')
-const fs = require('fs');
-const yaml = require('js-yaml');
-const { Wallets, Gateway } = require('fabric-network');
+const fs = require('fs')
+const yaml = require('js-yaml')
+const { Wallets, Gateway } = require('fabric-network')
 const path = require('path')
-const User = require('../../contract/lib/user.js');
+const User = require('../../contract/lib/user.js')
 
 class RentalController {
 
   async queryUser(ctx) {
     // A wallet stores a collection of identities for use
-    const wallet = await Wallets.newFileSystemWallet('../../identity/user/isabella/wallet');
+    const wallet = await Wallets.newFileSystemWallet('../../identity/user/isabella/wallet')
 
     // A gateway defines the peers used to access Fabric networks
-    const gateway = new Gateway();
+    const gateway = new Gateway()
 
     // Main try/catch block
     try {
 
       // Specify userName for network access
-      // const userName = 'isabella.issuer@magnetocorp.com';
-      const userName = 'isabella';
+      // const userName = 'isabella.issuer@magnetocorp.com'
+      const userName = 'isabella'
 
-      // Load connection profile; will be used to locate a gateway
-      let connectionProfile = yaml.safeLoad(fs.readFileSync('../../gateway/connection-profile.yaml', 'utf8'));
+      // Load connection profile will be used to locate a gateway
+      let connectionProfile = yaml.safeLoad(fs.readFileSync('../../gateway/connection-profile.yaml', 'utf8'))
 
-      // Set connection options; identity and wallet
+      // Set connection options identity and wallet
       let connectionOptions = {
         identity: userName,
         wallet: wallet,
         discovery: { enabled: false, asLocalhost: false }
-      };
+      }
 
-      await gateway.connect(connectionProfile, connectionOptions);
+      await gateway.connect(connectionProfile, connectionOptions)
 
-      const network = await gateway.getNetwork('rental');
+      const network = await gateway.getNetwork('rental')
 
-      const contract = await network.getContract('rental');
+      const contract = await network.getContract('rental')
 
-      const issueResponse = await contract.submitTransaction('queryUser', 'user01');
+      const queryResponse = await contract.submitTransaction('queryUser', 'user01')
 
       // process response
-      console.log('Process issue transaction response.'+issueResponse);
+      console.log('Process issue transaction response.' + queryResponse)
 
-      let paper = User.fromBuffer(issueResponse);
+      ctx.body = User.fromBuffer(queryResponse)
 
-      ctx.body = issueResponse
-
-      console.log(`${paper.issuer} commercial paper : ${paper.paperNumber} successfully issued for value ${paper.faceValue}`);
-      console.log('Transaction complete.');
+      console.log('Transaction complete.')
 
     } catch (error) {
 
-      console.log(`Error processing transaction. ${error}`);
-      console.log(error.stack);
+      console.log(`Error processing transaction. ${error}`)
+      console.log(error.stack)
 
     } finally {
 
       // Disconnect from the gateway
-      console.log('Disconnect from Fabric gateway.');
-      gateway.disconnect();
+      console.log('Disconnect from Fabric gateway.')
+      gateway.disconnect()
 
     }
   }
